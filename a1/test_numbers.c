409a9cc041b6beea622416e8cb788de5a82bd179
olekkr <olekkr@outlook.com>
2022-12-01 22:29:58 +0100

    shortened.

--- a1/test_numbers.c
+++ a1/test_numbers.c
@@ -1,29 +1,6 @@
 #include <stdlib.h>
 #include "numbers.h"
 
-struct bits8 test0 = {       
-    {false},{false},{false},{false},
-    {false},{false},{false},{false},
-};
-
-struct bits8 test1 = {       
-    {true},{false},{false},{false},
-    {false},{false},{false},{false},
-};
-
-struct bits8 test2 = {       
-    {true}, {false}, {false}, {true}, 
-    {true}, {false}, {false}, {true}
-};
-
-bool test_test(unsigned char n){
-    return false;
-}
-
-bool test_test2(unsigned char n, unsigned char nn){
-    return false;
-}
-
 int test_on_domain(int a, bool(*test)(unsigned char)){
     int fails = 0;
     for (int i = 0; i < 256; i++){
@@ -68,24 +45,9 @@ bool test_multiplication(unsigned char n, unsigned char nn){
 
 
 int main() {
-    struct bits8 bits = bits8_not(test1);
-    bits8_print(bits);
-    printf("\n");
-    bits8_print(bits8_from_int((unsigned int)2));
-
-    printf("\n");
-    printf("%d", bits8_to_int(test2));
-    
-    printf("\n");
-    bits8_print(bits8_add(test2, test1));
-    printf("\n");
-    
-    printf("testing conversions ... finshed with %d failures.\n", test_on_domain(255, test_conversion));
-    
+    printf("testing conversions ... finshed with %d failures.\n", test_on_domain(255, test_conversion)); 
     printf("testing addition ... finshed with %d failures.\n", test_on_domain2(64, test_addition));
-
     printf("testing negation ... finshed with %d failures.\n", test_on_domain(256, test_negation));
-
     // might not work due to overflows.
     printf("testing multiplication ... finshed with %d failures.\n", test_on_domain2(256, test_multiplication));
 
