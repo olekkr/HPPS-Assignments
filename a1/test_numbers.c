c0c100e2525add47b15e1278da56f41d6157ed4f
olekkr <olekkr@outlook.com>
2022-12-03 15:49:37 +0100

    fixed substraction test.

--- a1/test_numbers.c
+++ a1/test_numbers.c
@@ -31,6 +31,10 @@ bool test_addition(unsigned char n, unsigned char nn){
     bool b = (n + nn) == bits8_to_int(bits8_add(bits8_from_int(n), bits8_from_int(nn)));
     return b;
     }
+bool test_minus(unsigned char n, unsigned char nn){
+    bool b = (unsigned char)(n - nn) == bits8_to_int(bits8_add(bits8_from_int(n), bits8_negate(bits8_from_int(nn))));
+    return b;
+    }
 
 bool test_negation(unsigned char n){
     signed char a = -((signed char)n) ;
@@ -46,7 +50,7 @@ bool test_multiplication(unsigned char n, unsigned char nn){
 int main() {
     printf("testing conversions ... finshed with %d failures.\n", test_on_domain(0, 255, test_conversion)); 
     printf("testing addition ... finshed with %d failures.\n", test_on_domain2(0, 64, test_addition));
-    printf("testing addition- ... finshed with %d failures.\n", test_on_domain2(-64, 0, test_addition));
+    printf("testing addition- ... finshed with %d failures.\n", test_on_domain2(0, 64, test_minus));
     printf("testing negation ... finshed with %d failures.\n", test_on_domain(0, 256, test_negation));
     printf("testing negation- ... finshed with %d failures.\n", test_on_domain(-256, 0, test_negation));
     // might not work due to overflows.
