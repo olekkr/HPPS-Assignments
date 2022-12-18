a5e987c117608772ecb9e8f2f6e9198ca3b86f1c
olekkr <olekkr@outlook.com>
2022-12-18 21:56:02 +0100

    was returning null pointer instead of closest, size was set to n*sizeof int instead of double

--- a2/src/bruteforce.c
+++ a2/src/bruteforce.c
@@ -4,6 +4,21 @@
 #include <assert.h>
 
 int* knn(int k, int d, int n, const double *points, const double* query) {
-  assert(0);
-}
 
+  int* closest =  malloc(k * sizeof(double));
+
+  //int candidate = 0;
+
+  for (int i =0; i< k ; i ++){
+    closest[i] = -1;
+  }
+  // skal den loope over 'candidate'? Skal funktionskaldet gemmes under et
+  // navn fx "closest= ..."
+
+  // måske skal n forbindes med candidates på en anden måde. forud for loopet nednefor.
+
+  for (int candidate = 0; candidate < n; candidate ++) {
+    insert_if_closer(k, d, points, closest,  query, candidate);
+  }
+  return closest;
+}
--- a2/src/util.c
+++ a2/src/util.c
@@ -4,12 +4,10 @@
 #include <assert.h>
 
 double distance(int d, const double *x, const double *y) {
-  assert(0);
   // min accumlerede værdi til brug i loopet initialiseres her?
   double acc = 0;
   int i;
-  double acc = 0;
-  for (i = 1; i <= d; i ++;){
+  for (i = 1; i <= d; i ++){
       acc = acc + pow(x[i] - y[i], 2);
   }
 
@@ -22,35 +20,28 @@ double distance(int d, const double *x, const double *y) {
 int insert_if_closer(int k, int d,
                      const double *points, int *closest, const double *query,
                      int candidate) {
-  assert(0);
   // altså noget med
   double candDist;
 
-// jeg skal indexerer ind i candiate
+  // jeg skal indexerer ind i candiate
 
-      candDist = distance(d, query, &points[candidate]);
+  candDist = distance(d, query, &points[candidate]);
 
-  for(int i = 1; i <= k; i ++;){
+  for(int i = 1; i <= k; i ++){
     if (closest[i] == -1) {
         closest[i] = candidate;
 
       return(1);
     }
-    // den her er ikke færdig
-    double closestDist = closestDist(d, query, &points[closest[i]]);
+    candDist = distance(d, query, &points[closest[i]]);
 
-    if (candDist < closestDist){
+    if (candDist < closest[candidate]){
         for (int j = k-1; j<= i; j--)  {
            closest[j] = closest[j-1];
          }
       closest[i] = candidate;
       return(1);
     }
-return(0);
-
+    return(0);
   }
-
-
-
-
 }
