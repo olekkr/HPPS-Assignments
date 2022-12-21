5d221b90142f10cbbd1fc557a139926cb33f7d26
olekkr <olekkr@outlook.com>
2022-12-21 16:42:33 +0100

    improved the makefile

--- a3/src/id_query_naive.c
+++ a3/src/id_query_naive.c
@@ -15,18 +15,31 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  // TODO
-  assert(0);
+  struct naive_data* d = malloc(sizeof(struct naive_data));
+  d->rs=rs;
+  d->n=n;
+  return d;
 }
 
 void free_naive(struct naive_data* data) {
-  // TODO
-  assert(0);
+  printf("data freed");
+  free(data);
+  return;
 }
 
 const struct record* lookup_naive(struct naive_data *data, int64_t needle) {
-  // TODO
-  assert(0);
+  const struct record* rs = data->rs;
+  int n = data->n;
+
+  struct record* result = malloc(sizeof(const struct record));
+  
+  for(int i = 0; i<n; i++){
+      if(rs[i].osm_id == needle){
+          memcpy(result, &rs[i], sizeof(struct record));
+          return result;
+      }
+  }
+  return NULL;
 }
 
 int main(int argc, char** argv) {
